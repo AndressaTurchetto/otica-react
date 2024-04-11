4d2bba27cb0035420592a979c92810ca7d14006d
andressa turchetto <and.turchetto@gmail.com>
2024-04-11 14:30:00 -0300

    Criando arquivos da secao Banner

--- src/componentes/Cabecalho/index.js
+++ src/componentes/Cabecalho/index.js
@@ -1,5 +1,10 @@
+import './estilos.css';
+
 export default function Cabecalho() {
     return (
-        
+        <section className="">
+            <div className='limitar-secao'>
+            </div>
+        </section>
     )
 }
