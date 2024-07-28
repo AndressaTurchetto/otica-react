a678b2716b28f36bb4d43238830c6bc5ed941793
andressa turchetto <and.turchetto@gmail.com>
2024-07-28 09:42:50 -0300

    Criando a secao de contato

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import <Cabecalho> from "../componentes/Cabecalho/index.js";
-
-export default function Home() {
-  return (
-    <Cabacalho />
-  );
-}
